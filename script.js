dca7a14f5504001ee01851d635b405a130af2673
ElSindicatoDelCrimen <167438353+ElSindicatoDelCrimen@users.noreply.github.com>
2024-04-18 15:22:13 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,41 @@
+const eventTime = new Date("2024-04-17T19:20:00").getTime(); // Asegúrate de ajustar esta fecha y hora al evento
+
+const countdownFunction = setInterval(function() {
+    const now = new Date().getTime();
+    const distance = eventTime - now;
+    let days = Math.floor(distance / (1000 * 60 * 60 * 24));
+    let hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    let minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+    let seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+    document.getElementById("countdown").innerHTML = days + "d " + hours + "h "
+    + minutes + "m " + seconds + "s ";
+
+    if (distance < 0) {
+        clearInterval(countdownFunction);
+        document.getElementById("countdown").innerHTML = "La clave de acceso al chat es: 1234"; // Asegúrate de ajustar este mensaje y la clave como consideres apropiado
+    }
+}, 1000);
+
+function checkPassword() {
+    const passwordInput = document.getElementById("password").value;
+    const secretPassword = "La contraseña es TONTOQUIENLOLEA"; // Asegúrate de que esto coincida con el mensaje revelado
+
+    if (passwordInput === secretPassword) {
+        window.location.href = 'pista.html';
+    } else {
+        alert("Contraseña incorrecta paqueteheroe, pringado, fuera de aquí.");
+    }
+}
+
+window.onload = function() {
+    const termsModal = document.getElementById('termsModal');
+    const contentContainer = document.querySelector('.container');
+
+    function acceptTerms() {
+        termsModal.style.display = 'none';
+        contentContainer.style.display = 'block';
+    }
+
+    window.acceptTerms = acceptTerms; // Hacer la función accesible globalmente para ser llamada desde el HTML
+}
