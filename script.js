f0a97450a0bea8862f8f1e30751c40e928cddaf5
ElSindicatoDelCrimen <167438353+ElSindicatoDelCrimen@users.noreply.github.com>
2024-04-18 15:47:27 +0200

    Update script.js

--- script.js
+++ script.js
@@ -39,17 +39,3 @@ window.onload = function() {
 
     window.acceptTerms = acceptTerms; // Hacer la función accesible globalmente para ser llamada desde el HTML
 }
-
-document.addEventListener('DOMContentLoaded', function() {
-    const messages = document.getElementById('chatMessages');
-    const newMessages = [
-        "<li><strong>Harley Quinn:</strong> ¡Necesito más explosivos! ¿Alguien puede compartir?</li>",
-        "<li><strong>Black Manta:</strong> He tomado el control de los siete mares, ¿alguien necesita un poco de agua?</li>"
-    ];
-
-    newMessages.forEach((msg, index) => {
-        setTimeout(() => {
-            messages.innerHTML += msg;
-        }, (index + 1) * 5000); // Añade mensajes cada 5 segundos
-    });
-});
